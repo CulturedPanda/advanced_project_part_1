1fff9dc8c3f191835c5bdcd5a07f13afa6ceebcc
Yuval <yuvaluner@gmail.com>
2022-03-30 18:35:40 +0300

    Removed useless comments

--- src/Forms/LoginForm/LoginFormComponents/UsernameField.js
+++ src/Forms/LoginForm/LoginFormComponents/UsernameField.js
@@ -2,11 +2,8 @@ function UsernameField(){
     return (
         <div>
             <div className="col-auto">
-                <label htmlFor="Email" className="form-label">Email</label>
-                <input type="email" id="Email" className="form-control" aria-describedby="email-help"></input>
-            </div>
-            <div className="col-auto">
-                <span id="email-help" className="form-text">Email must be an email</span>
+                <label htmlFor="username" className="form-label">Username</label>
+                <input type="text" id="username" className="form-control" aria-describedby="email-help"></input>
             </div>
         </div>
     )
