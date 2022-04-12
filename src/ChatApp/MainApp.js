6417e696daf9fd2cb92054763430326987281a02
Yuval <yuvaluner@gmail.com>
2022-04-12 23:28:14 +0300

    Passing files along so we can test on different sized screens

--- src/ChatApp/MainApp.js
+++ src/ChatApp/MainApp.js
@@ -3,7 +3,7 @@ import Sidebar from "./Sidebar/Sidebar";
 
 function MainApp({props}) {
     return (
-        <div className="container-fluid p-5 pb-2">
+        <div className="container-fluid p-5 pb-2" id="main-app-div">
             <div className="row">
                 <Sidebar setLogIn={props.setLogIn}/>
                 <Conversation/>
